091bf77ab1d0fc1d54db661f6e97a00326f1dfac
Arseniy Pavlenko <h0x91b@gmail.com>
2016-04-12 13:04:23 +0300

    [=] Hello C++ from JS

--- src/scripting-v8.cc
+++ src/scripting-v8.cc
@@ -14,6 +14,7 @@ using namespace v8;
 
 v8::Platform* platform;
 sds wrapped_script;
+Isolate* isolate;
 
 class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
  public:
@@ -25,6 +26,21 @@ class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
     virtual void Free(void* data, size_t) { zfree(data); }
 };
 
+void Hello(const v8::FunctionCallbackInfo<v8::Value>& args) {
+    bool first = true;
+    for (int i = 0; i < args.Length(); i++) {
+        v8::HandleScope handle_scope(args.GetIsolate());
+        if (first) {
+            first = false;
+        } else {
+            printf(" ");
+        }
+        v8::String::Utf8Value str( args[i]->ToString() );
+        printf("%s", *str);
+    }
+    printf("\n");
+    fflush(stdout);
+}
 
 void initV8() {
     // Initialize V8.
@@ -34,10 +50,15 @@ void initV8() {
     V8::InitializePlatform(::platform);
     V8::Initialize();
     wrapped_script = sdsempty();
+    ArrayBufferAllocator allocator;
+    Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator = &allocator;
+    isolate = Isolate::New(create_params);
     serverLog(LL_WARNING,"V8 initialized");
 }
 
 void shutdownV8() {
+    isolate->Dispose();
     V8::Dispose();
     V8::ShutdownPlatform();
     delete ::platform;
@@ -46,53 +67,51 @@ void shutdownV8() {
 }
 
 void jsEvalCommand(client *c) {
-    ArrayBufferAllocator allocator;
-    Isolate::CreateParams create_params;
-    create_params.array_buffer_allocator = &allocator;
-    Isolate* isolate = Isolate::New(create_params);
+    Isolate::Scope isolate_scope(isolate);
+    HandleScope handle_scope(isolate);
     
-    {
-        Isolate::Scope isolate_scope(isolate);
-        HandleScope handle_scope(isolate);
-        Local<Context> context = Context::New(isolate);
-        Context::Scope context_scope(context);
-        
-        //flush previous data
-        wrapped_script[0] = '\0';
-        sdsupdatelen(wrapped_script);
-        
-        wrapped_script = sdscatlen(wrapped_script, "JSON.stringify((function wrap(){\n", 33);
-        wrapped_script = sdscatlen(wrapped_script, (const char*)c->argv[1]->ptr, sdslen((const sds)c->argv[1]->ptr));
-        wrapped_script = sdscatlen(wrapped_script, "\n})(), null, '\\t');", 19);
-        
-        Local<String> source = String::NewFromUtf8(
-            isolate, 
-            wrapped_script, 
-            NewStringType::kNormal, 
-            sdslen((const sds)c->argv[1]->ptr) + 33 + 19
-        ).ToLocalChecked();
-        TryCatch trycatch(isolate);
-        Local<Script> script;
-        
-        if(!Script::Compile(context, source).ToLocal(&script)) {
-            String::Utf8Value error(trycatch.Exception());
-            serverLog(LL_WARNING, "JS Compile exception %s\n", *error);
-            addReplyErrorFormat(c, "JS Compile exception %s", *error);
-            return;
-        }
-        Local<Value> result;
-        if(!script->Run(context).ToLocal(&result)) {
-            String::Utf8Value error(trycatch.Exception());
-            serverLog(LL_WARNING, "JS Runtime exception %s\n", *error);
-            addReplyErrorFormat(c, "JS Runtime exception %s", *error);
-        } else {
-            String::Utf8Value utf8(result);
-            robj *o;
-            o = createObject(OBJ_STRING,sdsnew(*utf8));
-            addReplyBulk(c,o);
-            decrRefCount(o);
-        }
+    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
+    global->Set(
+        v8::String::NewFromUtf8(isolate, "Hello", v8::NewStringType::kNormal).ToLocalChecked(),
+        v8::FunctionTemplate::New(isolate, Hello)
+    );
+    
+    Local<Context> context = Context::New(isolate, NULL, global);
+    Context::Scope context_scope(context);
+    
+    //flush previous data
+    wrapped_script[0] = '\0';
+    sdsupdatelen(wrapped_script);
+    
+    wrapped_script = sdscatlen(wrapped_script, "JSON.stringify((function wrap(){\n", 33);
+    wrapped_script = sdscatlen(wrapped_script, (const char*)c->argv[1]->ptr, sdslen((const sds)c->argv[1]->ptr));
+    wrapped_script = sdscatlen(wrapped_script, "\n})(), null, '\\t');", 19);
+    
+    Local<String> source = String::NewFromUtf8(
+        isolate, 
+        wrapped_script, 
+        NewStringType::kNormal, 
+        sdslen((const sds)c->argv[1]->ptr) + 33 + 19
+    ).ToLocalChecked();
+    TryCatch trycatch(isolate);
+    Local<Script> script;
+    
+    if(!Script::Compile(context, source).ToLocal(&script)) {
+        String::Utf8Value error(trycatch.Exception());
+        serverLog(LL_WARNING, "JS Compile exception %s\n", *error);
+        addReplyErrorFormat(c, "JS Compile exception %s", *error);
+        return;
     }
-    
-    isolate->Dispose();
+    Local<Value> result;
+    if(!script->Run(context).ToLocal(&result)) {
+        String::Utf8Value error(trycatch.Exception());
+        serverLog(LL_WARNING, "JS Runtime exception %s\n", *error);
+        addReplyErrorFormat(c, "JS Runtime exception %s", *error);
+    } else {
+        String::Utf8Value utf8(result);
+        robj *o;
+        o = createObject(OBJ_STRING,sdsnew(*utf8));
+        addReplyBulk(c,o);
+        decrRefCount(o);
+    }
 }
